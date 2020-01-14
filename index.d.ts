1edfc6f47f1167b38db3577e4999178c351cf7db
raohong <724736528@qq.com>
2020-01-14 21:56:15 +0800

    更新配置与申明

---
+++ index.d.ts
@@ -0,0 +1,60 @@
+interface SizeAndPositionOfItemData {
+  index: number;
+  style: React.CSSProperties;
+}
+
+type ItemSize = number | number[] | ((index: number) => number);
+
+export interface VirutalListItemData<T = any>
+  extends SizeAndPositionOfItemData {
+  item: T;
+}
+
+type MiniItemSizeValue = string | number;
+
+type MiniItemSize =
+  | MiniItemSizeValue
+  | MiniItemSizeValue[]
+  | ((index: number) => MiniItemSizeValue);
+
+type VirutalListDataManagerChangeHandler<T> = (
+  items: VirutalListItemData<T>[]
+) => void;
+type VirutalListDataManagerUpdater<T> = (data: T[]) => VirutalListItemData<T>[];
+
+interface VirutalListDataManagerOptions<T> {
+  itemSize?: MiniItemSize;
+  estimatedSize?: number;
+  stickyIndices?: number[];
+  overscan?: number;
+  column?: number;
+  onChange: VirutalListDataManagerChangeHandler<T>;
+}
+interface ILoadStatusResult<T> {
+  id: string;
+  clearAndAddData: (...values: T[]) => void;
+}
+interface VirutalListDataManagerState<T> {
+  data: T[];
+  itemSize: ItemSize;
+  overscan: number;
+  column: number;
+  itemCount: number;
+  estimatedSize: number;
+  stickyIndices: number[];
+  onChange: VirutalListDataManagerChangeHandler<T>;
+}
+
+export class VirutalListDataManager<T = any> {
+  constructor(options: VirutalListDataManagerOptions<T>);
+
+  updateConfig: (config: Partial<VirutalListDataManagerOptions<T>>) => void;
+  getItemCount: () => number;
+  clear: () => void;
+  push: (...value: T[]) => number;
+  set: (...value: T[]) => void;
+  splice: (start: number, deleteCount: number, ...items: T[]) => T[];
+  get: () => T[];
+  pop: () => T | undefined;
+  forceUpdate: () => void;
+}
